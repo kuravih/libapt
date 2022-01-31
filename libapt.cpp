39cbd3f028a69bb6c4b807081e5c48884170f234
Kuravi Hewawasam <kuravih@gmail.com>
2022-01-31 13:47:14 -0500

    Replace string arrays with const char* arrays

--- libapt.cpp
+++ libapt.cpp
@@ -582,13 +582,13 @@ void aptserial::APTDevice::getChannelEnableState(PZ_STATE& _state, const PZ_CHAN
 
 // ====================================================================================================================
 const std::string aptserial::channelToString(const PZ_CHANNEL _channel) {
-  std::string channelStrings[(uint)PZ_CHANNEL::CHANNEL_N_CHANNELS] = PZ_CHANNEL_LABELS;
-  return channelStrings[(uint)_channel];
+  const char* channelStrings[(uint)PZ_CHANNEL::CHANNEL_N_CHANNELS] = PZ_CHANNEL_LABELS;
+  return std::string(channelStrings[(uint)_channel]);
 }
 
 
 const bool aptserial::stringToChannel(const std::string _channelString, PZ_CHANNEL& _channel) {
-  std::string channelStrings[(uint)PZ_CHANNEL::CHANNEL_N_CHANNELS] = PZ_CHANNEL_LABELS;
+  const char* channelStrings[(uint)PZ_CHANNEL::CHANNEL_N_CHANNELS] = PZ_CHANNEL_LABELS;
   auto itChannel = std::find(std::begin(channelStrings), std::end(channelStrings), _channelString);
   if (itChannel != std::end(channelStrings)) {
     _channel = (PZ_CHANNEL)(uint)std::distance(std::begin(channelStrings), itChannel);
@@ -599,13 +599,13 @@ const bool aptserial::stringToChannel(const std::string _channelString, PZ_CHANN
 
 
 const std::string aptserial::stateToString(const PZ_STATE _state) {
-  std::string stateStrings[(uint)PZ_STATE::STATE_N_STATES] = PZ_STATE_LABELS;
-  return stateStrings[(uint)_state];
+  const char* stateStrings[(uint)PZ_STATE::STATE_N_STATES] = PZ_STATE_LABELS;
+  return std::string(stateStrings[(uint)_state]);
 }
 
 
 const bool aptserial::stringToState(const std::string _stateString, PZ_STATE& _state) {
-  std::string stateStrings[(uint)PZ_STATE::STATE_N_STATES] = PZ_STATE_LABELS;
+  const char* stateStrings[(uint)PZ_STATE::STATE_N_STATES] = PZ_STATE_LABELS;
   auto itState = std::find(std::begin(stateStrings), std::end(stateStrings), _stateString);
   if (itState != std::end(stateStrings)) {
     _state = (PZ_STATE)(uint)std::distance(std::begin(stateStrings), itState);
@@ -616,13 +616,13 @@ const bool aptserial::stringToState(const std::string _stateString, PZ_STATE& _s
 
 
 const std::string aptserial::voltageRangeToString(const PZ_VOLTAGE_RANGE _vRange) {
-  std::string vRangeStrings[(uint)PZ_VOLTAGE_RANGE::VOLTAGE_RANGE_N_RANGES] = PZ_VOLTAGE_RANGE_LABELS;
-  return vRangeStrings[(uint)_vRange];
+  const char* vRangeStrings[(uint)PZ_VOLTAGE_RANGE::VOLTAGE_RANGE_N_RANGES] = PZ_VOLTAGE_RANGE_LABELS;
+  return std::string(vRangeStrings[(uint)_vRange]);
 }
 
 
 const bool aptserial::stringToVoltageRange(const std::string _vRangeString, PZ_VOLTAGE_RANGE& _vRange) {
-  std::string vRangeStrings[(uint)PZ_VOLTAGE_RANGE::VOLTAGE_RANGE_N_RANGES] = PZ_VOLTAGE_RANGE_LABELS;
+  const char* vRangeStrings[(uint)PZ_VOLTAGE_RANGE::VOLTAGE_RANGE_N_RANGES] = PZ_VOLTAGE_RANGE_LABELS;
   auto itVRange = std::find(std::begin(vRangeStrings), std::end(vRangeStrings), _vRangeString);
   if (itVRange != std::end(vRangeStrings)) {
     _vRange = (PZ_VOLTAGE_RANGE)(uint)std::distance(std::begin(vRangeStrings), itVRange);
@@ -633,13 +633,13 @@ const bool aptserial::stringToVoltageRange(const std::string _vRangeString, PZ_V
 
 
 const std::string aptserial::analogInputSourceToString(const PZ_ANALOG_INPUT_SOURCE _analogInputSource) {
-  std::string analogInputSourceStrings[(uint)PZ_ANALOG_INPUT_SOURCE::ANALOG_INPUT_SOURCE_N_SOURCES] = PZ_ANALOG_INPUT_SOURCE_LABELS;
-  return analogInputSourceStrings[(uint)_analogInputSource];
+  const char* analogInputSourceStrings[(uint)PZ_ANALOG_INPUT_SOURCE::ANALOG_INPUT_SOURCE_N_SOURCES] = PZ_ANALOG_INPUT_SOURCE_LABELS;
+  return std::string(analogInputSourceStrings[(uint)_analogInputSource]);
 }
 
 
 const bool aptserial::stringToAnalogInputSource(const std::string _analogInputSourceString, PZ_ANALOG_INPUT_SOURCE& _analogInputSource) {
-  std::string analogInputSourceStrings[(uint)PZ_ANALOG_INPUT_SOURCE::ANALOG_INPUT_SOURCE_N_SOURCES] = PZ_ANALOG_INPUT_SOURCE_LABELS;
+  const char* analogInputSourceStrings[(uint)PZ_ANALOG_INPUT_SOURCE::ANALOG_INPUT_SOURCE_N_SOURCES] = PZ_ANALOG_INPUT_SOURCE_LABELS;
   auto itAnalogInputSource = std::find(std::begin(analogInputSourceStrings), std::end(analogInputSourceStrings), _analogInputSourceString);
   if (itAnalogInputSource != std::end(analogInputSourceStrings)) {
     _analogInputSource = (PZ_ANALOG_INPUT_SOURCE)(uint)std::distance(std::begin(analogInputSourceStrings), itAnalogInputSource);
@@ -650,13 +650,13 @@ const bool aptserial::stringToAnalogInputSource(const std::string _analogInputSo
 
 
 const std::string aptserial::inputVoltageSourceToString(const PZ_INPUT_VOLTAGE_SOURCE _inputVSource) {
-  std::string inputVSourceStrings[(uint)PZ_INPUT_VOLTAGE_SOURCE::INPUT_VOLTAGE_SOURCE_N_SOURCES] = PZ_INPUT_VOLTAGE_SOURCE_LABELS;
-  return inputVSourceStrings[(uint)_inputVSource];
+  const char* inputVSourceStrings[(uint)PZ_INPUT_VOLTAGE_SOURCE::INPUT_VOLTAGE_SOURCE_N_SOURCES] = PZ_INPUT_VOLTAGE_SOURCE_LABELS;
+  return std::string(inputVSourceStrings[(uint)_inputVSource]);
 }
 
 
 const bool aptserial::stringToInputVoltageSource(const std::string _inputVSourceString, PZ_INPUT_VOLTAGE_SOURCE& _inputVSource) {
-  std::string inputVSourceStrings[(uint)PZ_INPUT_VOLTAGE_SOURCE::INPUT_VOLTAGE_SOURCE_N_SOURCES] = PZ_INPUT_VOLTAGE_SOURCE_LABELS;
+  const char* inputVSourceStrings[(uint)PZ_INPUT_VOLTAGE_SOURCE::INPUT_VOLTAGE_SOURCE_N_SOURCES] = PZ_INPUT_VOLTAGE_SOURCE_LABELS;
   auto itInputVSource = std::find(std::begin(inputVSourceStrings), std::end(inputVSourceStrings), _inputVSourceString);
   if (itInputVSource != std::end(inputVSourceStrings)) {
     _inputVSource = (PZ_INPUT_VOLTAGE_SOURCE)(uint)std::distance(std::begin(inputVSourceStrings), itInputVSource);
@@ -667,13 +667,13 @@ const bool aptserial::stringToInputVoltageSource(const std::string _inputVSource
 
 
 const std::string aptserial::positionControlModeToString(const PZ_POSITION_CONTROL_MODE _positionCtrlMode) {
-  std::string positionCtrlModeStringStrings[(uint)PZ_POSITION_CONTROL_MODE::POSITION_CONTROL_MODE_N_MODES] = PZ_POSITION_CONTROL_MODE_LABELS;
-  return positionCtrlModeStringStrings[(uint)_positionCtrlMode];
+  const char* positionCtrlModeStringStrings[(uint)PZ_POSITION_CONTROL_MODE::POSITION_CONTROL_MODE_N_MODES] = PZ_POSITION_CONTROL_MODE_LABELS;
+  return std::string(positionCtrlModeStringStrings[(uint)_positionCtrlMode]);
 }
 
 
 const bool aptserial::stringToPositionControlMode(const std::string _positionCtrlModeString, PZ_POSITION_CONTROL_MODE& _positionCtrlMode) {
-  std::string positionCtrlModeStringStrings[(uint)PZ_POSITION_CONTROL_MODE::POSITION_CONTROL_MODE_N_MODES] = PZ_POSITION_CONTROL_MODE_LABELS;
+  const char* positionCtrlModeStringStrings[(uint)PZ_POSITION_CONTROL_MODE::POSITION_CONTROL_MODE_N_MODES] = PZ_POSITION_CONTROL_MODE_LABELS;
   auto itPositionControlModeSource = std::find(std::begin(positionCtrlModeStringStrings), std::end(positionCtrlModeStringStrings), _positionCtrlModeString);
   if (itPositionControlModeSource != std::end(positionCtrlModeStringStrings)) {
     _positionCtrlMode = (PZ_POSITION_CONTROL_MODE)(uint)std::distance(std::begin(positionCtrlModeStringStrings), itPositionControlModeSource);
